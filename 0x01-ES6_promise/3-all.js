c0e98a2ff752cadddf0c619f69af6ec6b301e153
Wakadaisho <paulwahome9@gmail.com>
2023-11-28 20:21:54 +0300

    Add return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from '../utils';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise.all([createUser(), uploadPhoto()])
     .then((res) => {
       console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`);
     })
