d51d4c4e6ab17398febfac352f4dae2fbcca994e
Wakadaisho <paulwahome9@gmail.com>
2023-11-30 21:10:55 +0300

    Add remaining tasks

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const cloneInstance = new this.constructor();
+
+    return cloneInstance;
+  }
+}
