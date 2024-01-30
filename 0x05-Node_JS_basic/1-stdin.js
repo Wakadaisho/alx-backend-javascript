90c1a3c97f3cb19bb43e41353ec72c577e99b556
Wakadaisho <paulwahome9@gmail.com>
2024-01-30 11:43:51 +0300

    Separate close event

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,6 +9,9 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+});
+
+rl.on('close', () => {
   console.log('This important software is now closing');
-  rl.close();
+  process.exit();
 });
