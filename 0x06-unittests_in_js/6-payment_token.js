94d5d57863cb904418da3a14f4ebacf05a433c0c
Wakadaisho <paulwahome9@gmail.com>
2024-01-31 21:44:30 +0300

    Add reject

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -2,6 +2,7 @@ function getPaymentTokenFromAPI(success) {
   if (success) {
     return Promise.resolve({ data: 'Successful response from API' });
   }
+  return Promise.reject(new Error('Invalid response from API'));
 }
 
 module.exports = getPaymentTokenFromAPI;
