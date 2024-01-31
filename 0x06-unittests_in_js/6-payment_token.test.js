bd86d9bdc31f78ef9b272647588765899b986f0a
Wakadaisho <paulwahome9@gmail.com>
2024-01-31 21:47:55 +0300

    Handle error

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -6,9 +6,13 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function () {
   it('getPaymentTokenFromAPI Promise true', function (done) {
-    getPaymentTokenFromAPI(true).then((result) => {
-      expect(result).to.eql({ data: 'Successful response from API' });
-      done();
-    });
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
   });
 });
